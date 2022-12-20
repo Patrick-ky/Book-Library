79268e1f321593f4377e0524be4b9158e6b2c709
Patrick-ky <120006515+Patrick-ky@users.noreply.github.com>
2022-12-20 10:28:27 +0800

    Add files via upload

--- SAMPLE/app.py
+++ SAMPLE/app.py
@@ -1,17 +1,88 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, flash
+import psycopg2
+import psycopg2.extras
 
 app = Flask(__name__)
 app.secret_key = "password"
 
+DB_HOST = "localhost"
+DB_NAME = "lib_sys_mydb"
+DB_USER = "postgres"
+DB_PASS = "password"
+ 
+conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST)
+
 @app.route('/')
-def home():
-    return render_template('homepage.html')
+def index():
+    cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    s = "SELECT * FROM students"
+    cur.execute(s) # Execute the SQL
+    list_users = cur.fetchall()
+    return render_template('index.html', list_users = list_users)
+
+@app.route('/students')
+def students():
+    return render_template("students.html")
+
+@app.route('/books')
+def books():
+    return render_template("books.html")
+
+@app.route('/add_student', methods=['POST'])
+def add_student():
+    cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    if request.method == 'POST':
+        fname = request.form['fname']
+        mname = request.form['mname']
+        lname = request.form['lname']
+        year_level = request.form['year_level']
+        section_name = request.form['section_name']
+        cur.execute("INSERT INTO students (id, fname, mname, lname, year_level, section_name) VALUES (%s, %s, %s, %s, %s, %s)", (id, fname, mname, lname, year_level, section_name))
+        conn.commit()
+        flash('Student Added successfully')
+        return redirect(url_for('Index'))
+ 
+@app.route('/edit/<id>', methods = ['POST', 'GET'])
+def get_employee(id):
+    cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+   
+    cur.execute('SELECT * FROM students WHERE id = %s', (id))
+    data = cur.fetchall()
+    cur.close()
+    print(data[0])
+    return render_template('edit.html', student = data[0])
+ 
+@app.route('/update/<id>', methods=['POST'])
+def update_student(id):
+    if request.method == 'POST':
+        fname = request.form['fname']
+        mname = request.form['mname']
+        lname = request.form['lname']
+        year_level = request.form['year_level']
+        section_name = request.form['section_name']
+        cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+        cur.execute("""
+            UPDATE students
+            SET fname = %s,
+                mname = %s,
+                lname = %s,
+                year_level = %s,
+                section_name = %s,
+            WHERE id = %s
+        """, (fname, mname, lname, year_level, section_name, id))
+        flash('Student Updated Successfully')
+        conn.commit()
+        return redirect(url_for('Index'))
+ 
+@app.route('/delete/<string:id>', methods = ['POST','GET'])
+def delete_student(id):
+    cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    cur.execute('DELETE FROM students WHERE id = {0}'.format(id))
+    conn.commit()
+    flash('Student Removed Successfully')
+    return redirect(url_for('Index'))
+ 
 
 if __name__ =="__main__":
     app.run(debug = True, port = 1234)
     
-@app.route('/home', methods = ['GET', 'POST'])
-def home():
-    data=request.form
-    print(data)
-    return render_template('homepage.html')
